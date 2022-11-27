b75ca47b799efe4deab6a46ab36c3918e64ce69b
Nadeemmuktar12345 <93705958+Nadeemmuktar12345@users.noreply.github.com>
2022-11-27 14:01:32 +0500

    Add files via upload

---
+++ Q-N0-32.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+  main()
+ {
+   string abc;
+ 	cout<<"Enter your color";
+ 	cin>>abc;
+ 	if(abc=="red")
+ 	{
+ 		cout<<"Please stop ";
+	 }
+	 else if(abc=="Yallow")
+	 {
+	 	cout<<"Please wait";
+	 	
+	 }
+	 else if(abc=="Yallow")
+	 {
+	 	cout<<"we are ready to go";
+	 }
+	 else 
+	 {
+	 	cout<<"Enter WRONG Number";
+	 }
+ 	
+ 	
+ 	
+ }
